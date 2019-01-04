466eb5e0bed07cc6f43c676d91f1e2a2bc24d555
rhimmelbauer <32716015+rhimmelbauer@users.noreply.github.com>
2019-01-04 14:03:34 -0800

    Update PixieBoardGPSLocation.py
    added fields

--- PixieBoardGPSLocation.py
+++ PixieBoardGPSLocation.py
@@ -13,7 +13,14 @@ STOP_SESSION = "echo 'AT+QGPSEND' | socat - /dev/ttyUSB2,cr"
 CONFIGURE_GPS_TRACKING = "echo 'AT+QGPS=1,30,50,0,1' | socat - /dev/ttyUSB2,cr"
 GET_GPS_LOCATION = "echo 'AT+QGPSLOC?' | socat - /dev/ttyUSB2,cr"
 GET_GPS_LOCATION_PRETTY = "echo 'AT+QGPSLOC=2' | socat - /dev/ttyUSB2,cr"
-
+GET_CELL_ID = "mmcli -m 0 --location-get | grep 'Cell ID' | awk '{print $4}'"
+GET_LOCATION_AREA_CODE = "mmcli -m 0 --location-get | grep 'Location area code' | awk '{print $5}'"
+GET_MOBILE_NETWORK_CODE = "mmcli -m 0 --location-get | grep 'Mobile network code' | awk '{print $5}'"
+GET_MOBILE_COUNTRY_CODE = "mmcli -m 0 --location-get | grep 'Mobile country code' | awk '{print $7}'"
+GET_IMEI = "mmcli -m 0 | grep 'imei' | awk '{print $4}'"
+GET_PROVIDER = "mmcli -m 0 | grep 'operator name' | awk '{print $4}'"
+GET_PROVIDER_ID = "mmcli -m 0 | grep 'operator id' | awk '{print $4}'"
+GET_SIGNAL_LEVEL = "mmcli -m 0 | grep 'signal quality' | awk '{print $4}'"
 
 class PixieBoardGPSLocation():		
 
@@ -32,6 +39,14 @@ class PixieBoardGPSLocation():
 		self.SpeedOverGroundKnots = ""
 		self.Date = ""
 		self.NumberOfSatellites = ""
+		self.CellId = ""
+		self.LocationAreaCode = ""
+		self.MobileNetworkCode = ""
+		self.MobileCountryCode = ""
+		self.IMEI = ""
+		self.Provider = ""
+		self.ProviderID = ""
+		self.SignalLevel = ""
 
 	def CheckModemStatus(self):
 		(command_output, error) = self.SendShellCommand(CHECK_MODEM_STATUS)
@@ -65,6 +80,55 @@ class PixieBoardGPSLocation():
 		else:
 			return False, command_output, error
 
+	def GetCellId(self, shell_command=GET_CELL_ID):
+		(command_output, error) = self.SendShellCommand(shell_command)
+		self.CellId = self.ParseCommandLineValue(command_output)
+		return self.CellId
+
+	def GetLocationAreaCode(self, shell_command=GET_LOCATION_AREA_CODE):
+		(command_output, error) = self.SendShellCommand(shell_command)
+		self.LocationAreaCode = self.ParseCommandLineValue(command_output)
+		return self.LocationAreaCode
+
+	def GetMobileNetworkCode(self, shell_command=GET_MOBILE_NETWORK_CODE):
+		(command_output, error) = self.SendShellCommand(shell_command)
+		self.MobileNetworkCode = self.ParseCommandLineValue(command_output)
+		return self.MobileNetworkCode
+
+	def GetMobileCountryCode(self, shell_command=GET_MOBILE_COUNTRY_CODE):
+		(command_output, error) = self.SendShellCommand(shell_command)
+		self.MobileCountryCode = self.ParseCommandLineValue(command_output)
+		return self.MobileCountryCode
+
+	def GetIMEI(self, shell_command=GET_IMEI):
+		(command_output, error) = self.SendShellCommand(shell_command)
+		self.IMEI = self.ParseCommandLineValue(command_output)
+		return self.IMEI
+
+	def GetProvider(self, shell_command=GET_PROVIDER):
+		(command_output, error) = self.SendShellCommand(shell_command)
+		self.Provider = self.ParseCommandLineValue(command_output)
+		return self.Provider
+
+	def GetProviderID(self, shell_command=GET_PROVIDER_ID):
+		(command_output, error) = self.SendShellCommand(shell_command)
+		self.ProviderID = self.ParseCommandLineValue(command_output)
+		return self.ProviderID
+
+	def GetSignalLevel(self, shell_command=GET_SIGNAL_LEVEL):
+		(command_output, error) = self.SendShellCommand(shell_command)
+		self.SignalLevel = self.ParseCommandLineValue(command_output)
+		return self.SignalLevel
+
+	def SetCellProviderLocationData(self):
+		self.GetCellId()
+		self.GetLocationAreaCode()
+		self.GetMobileNetworkCode()
+		self.GetMobileCountryCode()
+		self.GetIMEI()
+		self.GetProvider()
+		self.GetProviderID()
+		self.GetSignalLevel()
 
 	def ConfigureGPSTracking(self, shell_command=CONFIGURE_GPS_TRACKING):
 		self.StopSession()
@@ -98,24 +162,54 @@ class PixieBoardGPSLocation():
 			else:
 				time.sleep(8)
 
+	def NAttemptsToGetLocationPretty(self, attempts):
+		attemptCounter = 0
+		while True:
+			attemptCounter = attemptCounter + 1
+			signalReady, raw, error = self.GetGPSLocationPretty()
+			if signalReady:
+				break
+			elif attemptCounter == attempts:
+				break
+			else:
+				time.sleep(8)
+
 	def SendShellCommand(self, shellCommand):
+		print(shellCommand)
 		command = subprocess.Popen([shellCommand], stdout=subprocess.PIPE, shell=True)
-		(command_output, error) = command.communicate(PIXIE_BOARDS_PASSWORD)
+		(command_output, error) = command.communicate()
+		print(command_output)
 		return command_output, error
 
 	def ParseGPSLocation(self, command_output):
+		print(command_output)
 		locationData = str(command_output).split(",")
-		self.UTCTime = locationData[0][30:]
-		self.Latitude = locationData[1]
-		self.Longitude = locationData[2]
-		self.HorizontalPrecision = locationData[3]
-		self.Altitude = locationData[4]
-		self.Fix = locationData[5]
-		self.Cog = locationData[6]
-		self.SpeedOverGroundKmH = locationData[7]
-		self.SpeedOverGroundKnots = locationData[8]
-		self.Date = locationData[9]
-		self.NumberOfSatellites = locationData[10][0:2]
+		try:
+			self.UTCTime = locationData[0][30:]
+			self.Latitude = locationData[1]
+			self.Longitude = locationData[2]
+			self.HorizontalPrecision = locationData[3]
+			self.Altitude = locationData[4]
+			self.Fix = locationData[5]
+			self.Cog = locationData[6]
+			self.SpeedOverGroundKmH = locationData[7]
+			self.SpeedOverGroundKnots = locationData[8]
+			self.Date = locationData[9]
+			self.NumberOfSatellites = locationData[10][0:2]
+		except IndexError:
+			print("index Error")
+			self.UTCTime = 0
+			self.Latitude = 0
+			self.Longitude = 0
+			self.HorizontalPrecision = 0
+			self.Altitude = 0
+			self.Fix = 0
+			self.Cog = 0
+			self.SpeedOverGroundKmH = 0
+			self.SpeedOverGroundKnots = 0
+			self.Date = 0
+			self.NumberOfSatellites = 0
+            
 
 	def ParseOKInMsg(self, command_output):
 		output = str(command_output)
@@ -131,5 +225,9 @@ class PixieBoardGPSLocation():
 			return True
 		else:
 			return False
+
+	def ParseCommandLineValue(self, value_raw):
+		value = str(value_raw)[3:-4]
+		return value
 
 
